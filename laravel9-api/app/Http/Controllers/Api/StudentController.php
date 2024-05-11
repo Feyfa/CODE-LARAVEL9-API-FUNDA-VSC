c49f25dc90311381e0b7d34ca42229a3e1e022ef
Feyfa <fisikamodern00@gmail.com>
2024-05-11 21:45:35 +0700

    ubah file di StudentController.php

--- laravel9-api/app/Http/Controllers/Api/StudentController.php
+++ laravel9-api/app/Http/Controllers/Api/StudentController.php
@@ -12,8 +12,10 @@ class StudentController extends Controller
     // untuk mendapatkan semua data dari tabel students
     public function index()
     {
+        // ambil semua data
         $students = Student::all();
         
+        // jika jumlah data nya lebih dari 0, maka berikan response keberhasilan, jika tidak maka berikan response kesalahan
         return ($students->count() > 0) ? 
                response()->json([ 'status' => 200, 'students' => $students ], 200) : 
                response()->json([ 'status' => 404, 'status_message' => 'No Record Found' ], 404) ;    
@@ -22,6 +24,7 @@ class StudentController extends Controller
     // untuk menambahkan data ke tabel students
     public function store(Request $request)
     {
+        // melakukan validasi
         $validator = Validator::make($request->all(), [
             'name' => ['required', 'string', 'max:191'],
             'course' => ['required', 'string', 'max:191'],
@@ -29,9 +32,11 @@ class StudentController extends Controller
             'phone' => ['required', 'digits:10'],
         ]);
 
+        // jika validasi gagal, berikan response kesalahan
         if($validator->fails()) 
             return response()->json([ 'status' => 422, 'errors' => $validator->messages() ], 422);
 
+        // masukan ke dalam database
         $students = Student::create([
             'name' => $request->name,
             'course' => $request->course,
@@ -39,6 +44,7 @@ class StudentController extends Controller
             'phone' => $request->phone,
         ]);
 
+        // jika berhasil dimasukan ke database, maka berikan response keberhasilan, jika tidak maka berikan response kesalahan
         return $students ? 
                response()->json([ 'status' => 200, 'message' => 'Student Added Created Successfully' ], 200) : 
                response()->json([ 'status' => 500, 'message' => 'Something Went Wrong' ], 500) ;
@@ -47,8 +53,10 @@ class StudentController extends Controller
     // untuk mendapatkan data dari tabel students bedasarkan id
     public function show($id)
     {
+        // cari student bedasarkan id nya
         $student = Student::find($id);
 
+        // jika ada, maka berikan response keberhasilan, jika tidak maka berikan response kesalahan
         return $student ? 
                response()->json([ 'status' => 200, 'student' => $student ], 200) : 
                response()->json([ 'status' => 404, 'message' => 'No Such Student Found!' ], 404) ;
@@ -89,7 +97,7 @@ class StudentController extends Controller
             'phone' => $request->phone,
         ]);
 
-        return response()->json([ 'status' => 200, 'message' => 'Student Added Created Successfully' ], 200);
+        return response()->json([ 'status' => 200, 'message' => 'Student Update Successfully' ], 200);
     }
 
     // untuk melakukan delete data di tabel students bedasarkan id
